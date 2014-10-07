0d9a772c7f279a000875d84f7b1ddca548b4df33
Jeff Cooper <jeffcooperjc@gmail.com>
2014-10-06 23:14:26 -0400

    Added icon configurations

--- WeatherApp/WeatherApp/Current.swift
+++ WeatherApp/WeatherApp/Current.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Current {
     
@@ -15,7 +16,7 @@ struct Current {
     var humidity: Double
     var precipProbability: Double
     var summary: String
-    var icon: String
+    var icon: UIImage?
     
     init(weatherDictionary: NSDictionary) {
         let currentWeather = weatherDictionary["currently"] as NSDictionary
@@ -24,10 +25,12 @@ struct Current {
         humidity = currentWeather["humidity"] as Double
         precipProbability = currentWeather["precipProbability"] as Double
         summary = currentWeather["summary"] as String
-        icon = currentWeather["icon"] as String
         
         let currentTimeIntValue = currentWeather["time"] as Int
         currentTime = dateStringFromUnixtime(currentTimeIntValue)
+        
+        let iconString = currentWeather["icon"] as String
+        icon = weatherIconFromString(iconString)
         
     }
     
@@ -38,5 +41,36 @@ struct Current {
         dateFormatter.timeStyle = .ShortStyle
         
         return dateFormatter.stringFromDate(weatherDate)
+    }
+    
+    func weatherIconFromString(stringIcon: String) -> UIImage {
+        var imageName: String
+        
+        switch stringIcon {
+        case "clear-day":
+            imageName = "clear-day"
+        case "clear-night":
+            imageName = "clear-night"
+        case "rain":
+            imageName = "rain"
+        case "snow":
+            imageName = "snow"
+        case "sleet":
+            imageName = "sleet"
+        case "wind":
+            imageName = "wind"
+        case "fog":
+            imageName = "fog"
+        case "cloudy":
+            imageName = "cloudy"
+        case "partly-cloudy-day":
+            imageName = "partly-cloudy"
+        case "partly-cloudy-night":
+            imageName = "cloudy-night"
+        default:
+            imageName = "default"
+        }
+        
+        return UIImage(named: imageName)
     }
 }
